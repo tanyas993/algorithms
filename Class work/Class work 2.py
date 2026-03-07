238e077784f2419b7635f87f8666b235f69b6cd9
tanya's <tanja.matuhina@yandex.ru>
2026-03-07 15:16:22 +1000

    change class work 2

---
+++ Class work/Class work 2.py
@@ -0,0 +1,29 @@
+#1 задание
+def sortedSquares(nums):
+    left, right = 0, len(nums)-1
+    while left < right:
+        nums[left] = abs(nums[left])**2
+        nums[right] = abs(nums[right])**2
+        left+=1
+        right-=1
+    return sorted(nums)
+
+nums = [-4, -1, 0, 3, 10]
+
+print(sortedSquares(nums))
+
+#2 задание
+def isAnagram( s, t):
+    return sorted(s) == sorted(t)
+
+s1 = 'anagram'
+t1 = 'nagaram'
+print (isAnagram( s1, t1))
+
+s2 = 'car'
+t2 = 'rat'
+print(isAnagram( s2, t2))
+
+
+
+
